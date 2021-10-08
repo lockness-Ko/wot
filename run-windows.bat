6a2fe562b32cf47b0c0cc7ccba534968a77a6c46
lockness-Ko <videolk9373@gmail.com>
2021-10-08 12:09:44 +1100

    Fix more revision issues!!???

--- run-windows.bat
+++ run-windows.bat
@@ -1,8 +1,8 @@
 # Windows version :)
 
 # Run the tor proxy using a simple tor docker container, to edit the settings edit the torrc file at wot/tor/torrc
-docker run --rm -dp 9050:9050 --name tor ghcr.io/lockness-ko/wottor:coffee
+docker run --rm -dp 5090:5090 --name tor ghcr.io/lockness-ko/wottor:cookie
 
 # Run the container on the host network to be able to connect to the locally running tor proxy
-docker run --rm --net host -it ghcr.io/lockness-ko/wot:coffee
+docker run --rm --net host -it ghcr.io/lockness-ko/wot:cookie
 # Using the --rm flag to remove the container once you exit
