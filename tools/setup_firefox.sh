0dd6cebed5050c4232e473109e9aca23f5dbc1a5
Lockness <42625905+lockness-Ko@users.noreply.github.com>
2021-10-24 10:15:58 +1100

    1984

--- tools/setup_firefox.sh
+++ tools/setup_firefox.sh
@@ -11,9 +11,10 @@ line='  firefox: "67.0.1"'
 version=$(echo $line | grep -o '".*"' | cut -d " " -f 1 | sed 's/"//g')
 
 FIREFOX_VERSION=$version
-# No idea why it makes something that isn't on the path, perhaps it is accounting for low-priv users :/
-#mkdir -p $HOME/bin
-pushd /usr/bin #$HOME/bin
+
+# had to put this here :/ 1984
+mkdir -p $HOME/bin
+pushd $HOME/bin
 curl -L -o firefox.tar.bz2 https://ftp.mozilla.org/pub/firefox/releases/$FIREFOX_VERSION/linux-x86_64/en-US/firefox-$FIREFOX_VERSION.tar.bz2
 bzip2 -d firefox.tar.bz2
 tar xf firefox.tar
