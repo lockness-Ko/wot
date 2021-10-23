b7f378e75ec07a8fcf36cda99bc6a8b62408d645
lockness-Ko <videolk9373@gmail.com>
2021-10-23 11:07:30 +1100

    lol I had no idea how to do batch comments

--- run-windows.bat
+++ run-windows.bat
@@ -1,8 +1,8 @@
-# Windows version :)
+:: Windows version :)
 
-# Run the tor proxy using a simple tor docker container, to edit the settings edit the torrc file at wot/tor/torrc
+:: Run the tor proxy using a simple tor docker container, to edit the settings edit the torrc file at wot/tor/torrc
 docker run --rm -dp 5090:5090 --name tor ghcr.io/lockness-ko/wottor:cookie
 
-# Run the container on the host network to be able to connect to the locally running tor proxy
+:: Run the container on the host network to be able to connect to the locally running tor proxy
 docker run --rm --net host -it ghcr.io/lockness-ko/wot:cookie
-# Using the --rm flag to remove the container once you exit
+:: Using the --rm flag to remove the container once you exit
