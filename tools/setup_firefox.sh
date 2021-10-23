88cb61ac0a7817fc03a8bc6e75cc9385feabb2b0
Lockness <42625905+lockness-Ko@users.noreply.github.com>
2021-10-24 10:12:27 +1100

    Update setup_firefox.sh

--- tools/setup_firefox.sh
+++ tools/setup_firefox.sh
@@ -17,6 +17,7 @@ pushd /usr/bin #$HOME/bin
 curl -L -o firefox.tar.bz2 https://ftp.mozilla.org/pub/firefox/releases/$FIREFOX_VERSION/linux-x86_64/en-US/firefox-$FIREFOX_VERSION.tar.bz2
 bzip2 -d firefox.tar.bz2
 tar xf firefox.tar
+chmod +x firefox
 popd
 
 firefox --version
