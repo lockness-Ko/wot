4612d88d09f906f6016bd8ca49c4d4f461f766d0
Lockness <42625905+lockness-Ko@users.noreply.github.com>
2021-11-09 10:53:55 +1100

    Make commands run in chroot

--- wotos/install.sh
+++ wotos/install.sh
@@ -19,40 +19,38 @@ pacstrap /mnt base base-devel linux linux-firmware sudo nano vi networkmanager d
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
-arch-chroot /mnt
-
 #in the chroot
-pacman -Sy grub reflector torctl tor lynx git python3 python3-pip python-is-pip zsh wget curl exa highlight xorg-xrandr xorg-server xorg-xinit terminator i3 numlockx #nitrogen
+arch-chroot /mnt pacman -Sy grub reflector torctl tor lynx git python3 python3-pip python-is-pip zsh wget curl exa highlight xorg-xrandr xorg-server xorg-xinit terminator i3 numlockx #nitrogen
 
-echo wot > /etc/hostname
+arch-chroot /mnt echo wot > /etc/hostname
 
 # Install stuff
-wget https://github.com/andrew-d/static-binaries/raw/master/binaries/linux/x86_64/ncat -O /usr/bin/ncat
-cd /root
-git clone https://github.com/Datalux/Osintgram.git
-git clone https://github.com/twintproject/twint.git
-git clone https://github.com/ReFirmLabs/binwalk.git
-wget https://raw.githubusercontent.com/lockness-Ko/wot/main/tools/zshrc -O /root/.zshrc
-wget https://raw.githubusercontent.com/lockness-Ko/wot/main/tools/motd -O /etc/motd
-wget https://raw.githubusercontent.com/lockness-Ko/wot/main/tools/hosts -O /etc/hosts
+arch-chroot /mnt wget https://github.com/andrew-d/static-binaries/raw/master/binaries/linux/x86_64/ncat -O /usr/bin/ncat
+arch-chroot /mnt cd /root
+arch-chroot /mnt git clone https://github.com/Datalux/Osintgram.git
+arch-chroot /mnt git clone https://github.com/twintproject/twint.git
+arch-chroot /mnt git clone https://github.com/ReFirmLabs/binwalk.git
+arch-chroot /mnt wget https://raw.githubusercontent.com/lockness-Ko/wot/main/tools/zshrc -O /root/.zshrc
+arch-chroot /mnt wget https://raw.githubusercontent.com/lockness-Ko/wot/main/tools/motd -O /etc/motd
+arch-chroot /mnt wget https://raw.githubusercontent.com/lockness-Ko/wot/main/tools/hosts -O /etc/hosts
 
 # Setup tor and torctl
-systemctl enable --now tor.service
-torctl start
+arch-chroot /mnt systemctl enable --now tor.service
+arch-chroot /mnt torctl start
 
-passwd
+arch-chroot /mnt passwd
 
-reflector --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+arch-chroot /mnt reflector --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 
-timedatectl set-timezone Australia/Sydney
+arch-chroot /mnt timedatectl set-timezone Australia/Sydney
 
-systemctl enable NetworkManager
+arch-chroot /mnt systemctl enable NetworkManager
 
-grub-install /dev/sda
+arch-chroot /mnt grub-install /dev/sda
 
-grub-mkconfig -o /boot/grub/grub.cfg
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
-exit
+# exit
 
 #out of chroot
 umount -R /mnt
