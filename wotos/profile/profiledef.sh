1b8494ac2d70c004d76d2971609d7fdfa4232f77
lockness-Ko <videolk9373@gmail.com>
2021-11-21 06:29:44 +0000

    Making wot os

--- wotos/profile/profiledef.sh
+++ wotos/profile/profiledef.sh
@@ -1,23 +1,18 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="wotos"
-iso_label="WOT_DONUT"
-iso_publisher="WOT <https://lockness-Ko.github.io/wot>"
-iso_application="WOT install ISO"
-iso_version="DONUT"
+iso_name="wot"
+iso_label="DONUT"
+iso_publisher="lockness Ko <https://lockness-Ko.github.io/wot>"
+iso_application="WOT linux"
+iso_version="2.0.0"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
-airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+airootfs_image_type="erofs"
+airootfs_image_tool_options=('-zlz4hc,12')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
-  ["/root"]="0:0:750"  ["/root/install.sh"]="0:0:750"
-  ["/root/chroot.sh"]="0:0:750"
-  ["/root/.automated_script.sh"]="0:0:755"
-  ["/usr/local/bin/choose-mirror"]="0:0:755"
-  ["/usr/local/bin/Installation_guide"]="0:0:755"
-  ["/usr/local/bin/livecd-sound"]="0:0:755")
+)
